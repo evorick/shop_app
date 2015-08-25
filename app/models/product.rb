4c8eef2d89d9659a7dff9c51c04e9d926e5031d0
evorick <rickkadlac@gmail.com>
2015-08-25 11:06:00 -0500

    added db associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
+
+  # has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/:style_missing.jpg"validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
