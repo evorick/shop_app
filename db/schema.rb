835805d44601698936579e78455f296ccb2427ec
evorick <rickkadlac@gmail.com>
2015-08-25 11:54:32 -0500

    added image magick

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825145049) do
+ActiveRecord::Schema.define(version: 20150825164012) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -32,8 +32,12 @@ ActiveRecord::Schema.define(version: 20150825145049) do
     t.text     "description"
     t.integer  "brand_id"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "products", ["brand_id"], name: "index_products_on_brand_id"
