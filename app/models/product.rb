322ef4096cc71bc8482525775a7625edbbf49921
evorick <rickkadlac@gmail.com>
2015-08-26 08:39:01 -0500

    Added image views

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,5 @@ class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
 
-  # has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/:style_missing.jpg"validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/:style_missing.jpg"validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
