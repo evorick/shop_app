bc3cf1bdc5077b2eb035b0a63e24512b85b43680
evorick <rickkadlac@gmail.com>
2015-08-27 08:51:36 -0500

    added a navbar for categories

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,5 +1,10 @@
 class StorefrontController < ApplicationController
   def index
+    if params[:cat_id]
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: params[:cat_id]).order(:name)
+    else
     @products = Product.order(:name).all
+    end
   end
 end
