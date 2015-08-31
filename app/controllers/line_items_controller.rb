529acebc24e334895d7b2086d2bc47e01e687fff
evorick <rickkadlac@gmail.com>
2015-08-31 09:05:52 -0500

    made changes in the line_item_controller so users cannot enter other users carts

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -77,6 +77,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id, :quantity, :price)
+      params.require(:line_item).permit(:product_id, :quantity, :price)
     end
 end
