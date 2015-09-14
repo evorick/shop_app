e32667d023080bb17137d58d72c47d5d1b903525
evorick <rickkadlac@gmail.com>
2015-09-14 08:39:09 -0500

    added amazon sdk gem to work with paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,17 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
+#paperclip gem config
+config.paperclip_defaults = {
+   :storage => :s3,
+   :s3_credentials => {
+     :bucket => ENV['S3_BUCKET_NAME'],
+     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+   }
+ }
+
+
   #Configuration for paperclip:
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
